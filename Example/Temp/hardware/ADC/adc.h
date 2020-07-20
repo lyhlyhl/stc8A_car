bb161fcc11aa939a34861867550ce3e8930ebefa
lyhlyhl <lyhlyhl@qq.com>
2020-07-21 04:40:48 +0800

    解决冲突

--- Example/Temp/hardware/ADC/adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-
-#include "stc8.h"
-#include "common.h"
-u16 GetADCResult(u8 ch);   
-
-#endif
--- Example/Temp/hardware/PWM/pwm.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PWM_H
-#define _PWM_H
-
-void Pwm_set(float L,float R);
-
-
-
-#endif
